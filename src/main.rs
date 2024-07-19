242dc4d05551e9ac71ecb389cfb7e94187e8a032
Jose-Prince <joseprince148@gmail.com>
2024-07-18 21:40:00 -0600

    Feat: Animation added

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,25 @@ use framebuffer::Framebuffer;
 use color::Color;
 use line::Line;
 use polygon::Polygon;
-use minifb::{Window, WindowOptions};
+use minifb::{Window, WindowOptions, Key};  // Aquí importamos la enum Key
 use std::time::Duration;
+
+fn render (framebuffer: &mut Framebuffer, x: isize) {
+    let background_color = Color::new(33, 33, 55);
+    let main_color = Color::new(255, 221, 221);
+
+    framebuffer.set_background_color(background_color);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(main_color);
+    framebuffer.point(x, 40);
+}
 
 fn main() {
     let window_width = 800;
     let window_height = 600;
 
-    let close_delay = Duration::from_secs(10);
+    let frame_delay = Duration::from_millis(0);
 
     let mut fb = Framebuffer::new(window_width, window_height);
 
@@ -32,23 +43,32 @@ fn main() {
         }
     };
 
-    let points: Vec<[isize; 2]> = vec![
-        [100, 100],
-        [400, 500],
-        [700, 300],
-    ];
+    let mut x = 1 as isize;  // Cambiado a isize
+    let mut speed = 1 as isize;  // Cambiado a isize
 
-    // Define los colores para el borde y el relleno
-    let border_color = Color::new(255, 0, 0); // Rojo
-    let fill_color = Color::new(0, 255, 0);   // Verde
-    
-    fb.polygon(&points, border_color, fill_color);
+    while window.is_open() {
+        if window.is_key_down(Key::Escape) {
+            break;
+        }
 
-    window.update_with_buffer(&fb.get_buffer(), window_width, window_height).unwrap();
+        if x as usize == window_width {
+            speed = -1;
+        } else if x == 0 {
+            speed = 1;
+        }
 
-    std::thread::sleep(close_delay);
+        x += speed;
 
-    // Save the framebuffer to a BMP file
+        // Aquí puedes agregar lógica adicional para actualizar tu framebuffer y dibujar cosas en él
+
+        render(&mut fb, x);
+
+        window.update_with_buffer(&fb.get_buffer(), window_width, window_height).unwrap();
+
+        std::thread::sleep(frame_delay);
+    }
+
+    // Guardar el framebuffer en un archivo BMP
     // if let Err(e) = fb.save_as_bmp("polygon.bmp") {
     //     eprintln!("Failed to write BMP file: {}", e);
     // }
