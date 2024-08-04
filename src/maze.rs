ec55dfc8664934604e438641ed431fd2a12eed33
Jose-Prince <joseprince148@gmail.com>
2024-08-04 16:10:42 -0600

    Feat: Player generation and position location define

--- src/maze.rs
+++ src/maze.rs
@@ -20,18 +20,24 @@ fn draw_cell(framebuffer: &mut Framebuffer, x0: usize, y0: usize, block_size: us
     }
 }
 
-pub fn render(framebuffer: &mut Framebuffer, file_path: &str) -> Result<()> {
+pub fn render(framebuffer: &mut Framebuffer, file_path: &str) -> Result<Option<(usize, usize)>> {
     let maze = load_maze(file_path)?;
     let rows = maze.len();
     let cols = maze[0].len();
     
     let block_size = std::cmp::min(framebuffer.get_width() / cols, framebuffer.get_height() / rows);
+    let mut player_position = None;
 
     for row in 0..rows {
         for col in 0..cols {
+            if maze[row][col] == 'p' {
+                let center_x = col * block_size + block_size / 2;
+                let center_y = row * block_size + block_size / 2;
+                player_position = Some((center_x, center_y));
+            }
             draw_cell(framebuffer, col * block_size, row * block_size, block_size, maze[row][col]);
         }
     }
 
-    Ok(())
+    Ok(player_position)
 }
